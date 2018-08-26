718d6362d9e3a6d9014eaff01f514cdedfb8034f
luoyaoxing <2539723916@qq.com>
2018-08-26 15:05:56 +0800

    完成邮件Mail功能类

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -1,6 +1,7 @@
 <?php
 namespace app\admin\controller;
 
+use phpmailer\Mail;
 use think\Controller;
 
 class Index extends Controller
@@ -12,5 +13,13 @@ class Index extends Controller
 
     public function welcome() {
         return '欢迎来到daiel开发的主后台管理平台';
+    }
+
+    public function test() {
+        $to = '3117748742@qq.com';
+        $title = '今天天气很好';
+        $content = '是的哈～～～～';
+        $ret = Mail::send($to, $title, $content);
+        return $ret;
     }
 }
---
+++ config/mail.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: luoyaoxing
+ * Date: 18-8-26
+ * Time: 下午2:01
+ */
+
+return [
+    //邮件服务器
+    'mail_host' => 'smtp.126.com',
+    //邮件端口
+    'mail_port' => '25',
+    //是否开启发邮件调试功能
+    'mail_debug' => 0,
+    //超时时间
+    'mail_timeout' => 60,
+    //debug调试输出类型
+    'mail_debugoutput' => 'json',
+    //邮箱账户名
+    'mail_username' => 'daniel123@126.com',
+    //客户端授权密码
+    'mail_password' => 'Zxcvb636968',
+    //发邮件的发送人
+    'mail_name' => 'daniel'
+];
