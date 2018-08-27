e7b946b8ff6949cf995f6569effc6638ee53ece4
luoyaoxing <2539723916@qq.com>
2018-08-27 23:28:14 +0800

    完成门店的管理

--- application/bis/common.php
+++ application/bis/common.php
@@ -1,1 +1,26 @@
 <?php
+
+function convertStatus($status) {
+    switch ($status) {
+        case 0: {
+            return "<span class='label label-warning radius'>待审核</span>";
+        }
+        case 1: {
+            return "<span class='label label-success radius'>正常</span>";
+        }
+        case -1: {
+            return "<span class='label label-danger radius'>弃用</span>";
+        }
+        default: {
+            return "<span class='label label-danger radius'>状态异常</span>";
+        }
+    }
+}
+
+function convertIsMain($is_main) {
+    if ($is_main == 1) {
+        return "<span class='label label-success radius'>是</span>";
+    }else {
+        return "<span class='label label-danger radius'>否</span>";
+    }
+}
--- application/common/model/BisLocation.php
+++ application/common/model/BisLocation.php
@@ -18,4 +18,23 @@ class BisLocation extends Model
             return false;
         }
     }
+
+    public function getBisLocation($bis_id, &$bisLocation) {
+        $condition = [
+            'bis_id' => $bis_id,
+            'status' => ['>=', 0]
+        ];
+
+        $order = ['id' => 'asc'];
+
+        $bisLocation = $this->where($condition)
+                            ->order($order)
+                            ->field(['id', 'name', 'logo', 'is_main', 'status', 'create_time'])
+                            ->select();
+        if($bisLocation == false) {
+            return false;
+        }else {
+            return true;
+        }
+    }
 }
